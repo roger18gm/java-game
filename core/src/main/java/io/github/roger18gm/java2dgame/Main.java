f5a8e5d330c41ecfdda5897d7891f524e7b278fb
lilysorensen <lilsoren@byui.edu>
2025-03-28 12:18:20 -0600

    merged menu screen with main and music

--- core/src/main/java/io/github/roger18gm/java2dgame/Main.java
+++ core/src/main/java/io/github/roger18gm/java2dgame/Main.java
@@ -2,38 +2,79 @@ package io.github.roger18gm.java2dgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.TimeUtils;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
-import javax.swing.JFrame;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-import java.awt.event.KeyEvent;
-import java.security.Key;
-
-//import java.lang.classfile.attribute.CharacterRangeTableAttribute;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    private Stage stage;
+    private Skin skin;
     private World world;
     private Box2DDebugRenderer debugRenderer;
     private SpriteBatch batch;
     private Character soldier;
     private MovePerson movePerson;  // Add a MovePerson instance
     private Background background;
+    private Sound sound;
+    private Music music;
+//    private NPC npc;
+
+    // The default is that the soldier hasn't attacked yet
+    private boolean attackSoundPlayed = false;
 
     @Override
     public void create() {
+
+        // UI components
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+//        skin = new Skin(Gdx.files.internal("ui/pixthulhuui/pixthulhu-ui.json"));
+        skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        // Add listeners to buttons
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Start the game
+                startGame();
+            }
+        });
+
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Exit the game
+                Gdx.app.exit();
+            }
+        });
+
+        // Create a table to organize the UI elements
+        Table table = new Table();
+        table.setFillParent(true);
+        table.center();
+        table.add(playButton).padBottom(20).row();
+        table.add(exitButton);
+
+        // Add the table to the stage
+        stage.addActor(table);
+
+        // physics
         world = new World(new Vector2(0,0), true);
         debugRenderer = new Box2DDebugRenderer();
         batch = new SpriteBatch();
@@ -43,34 +84,67 @@ public class Main extends ApplicationAdapter {
 
         // Create MovePerson instance and pass the soldierWalking character to it
         movePerson = new MovePerson(soldier);
-        Gdx.input.setInputProcessor(movePerson);
+//        Gdx.input.setInputProcessor(movePerson);
+
+        // Music
+         sound = Gdx.audio.newSound((Gdx.files.internal("07_human_atk_sword_2.mp3")));
+         music = Gdx.audio.newMusic(Gdx.files.internal("Sketchbook 2024-11-07.ogg"));;
+
+        music.setVolume(0.2f);
+        music.setLooping(true);
+        music.play();
+    }
+    void startGame() {
+        // Start the game
+        System.out.println("Game started!");
+        stage.clear(); // Clear the stage
+        Gdx.input.setInputProcessor(movePerson); // Set the input processor to MovePerson
     }
 
     @Override
     public void render() {
 
-        float deltaTime = Gdx.graphics.getDeltaTime();
-
-        // Call MovePerson's Move() method to update the character's position
-        movePerson.update(deltaTime);
-
+        // Default background color
         ScreenUtils.clear(0.08f, 0.07f, 0.23f, 1.0f); // RGB (255, 192, 203) -> (1.0, 0.75, 0.80)
 
-        batch.begin();
-        background.render(batch);
-        // Render the current character depending on the state (idle or walking)
-        soldier.render(batch);
-        batch.end();
-        world.step(1/60f, 6, 2); // Step the physics world
-        debugRenderer.render(world, batch.getProjectionMatrix());
+        if (Gdx.input.getInputProcessor() == stage) {
+            // Draw the stage
+            stage.act();
+            stage.draw();
+//            return;
+        }
+        else{
+            // Plays sword sound only once per click
+            if (movePerson.isAttacking() && !attackSoundPlayed) {
+                sound.play(1.0f); // Play sound only once per click
+                attackSoundPlayed = true; // Because the soldier has attacked once it won't repeat the sound
+            } else if (!movePerson.isAttacking()) {
+                attackSoundPlayed = false; // Reset so sound can play on the next attack
+            }
+
+            float deltaTime = Gdx.graphics.getDeltaTime();
+            // Call MovePerson's Move() method to update the character's position
+            movePerson.update(deltaTime);
+            batch.begin();
+            background.render(batch);
+            // Render the current character depending on the state (idle or walking)
+            soldier.render(batch);
+            batch.end();
+            world.step(1/60f, 6, 2); // Step the physics world
+            debugRenderer.render(world, batch.getProjectionMatrix());
+        }
     }
 
     @Override
     public void dispose() {
+        stage.dispose();
+        skin.dispose();
         batch.dispose();
         soldier.dispose();
         background.dispose();
         world.dispose();
         debugRenderer.dispose();
+        music.dispose();
+        sound.dispose();
     }
 }
--- core/src/main/java/io/github/roger18gm/java2dgame/MovePerson.java
+++ core/src/main/java/io/github/roger18gm/java2dgame/MovePerson.java
@@ -2,6 +2,8 @@ package io.github.roger18gm.java2dgame;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.audio.Sound;
 
 import java.awt.event.KeyEvent;
 
@@ -9,21 +11,31 @@ import java.awt.event.KeyEvent;
 public class MovePerson  implements InputProcessor {
 
     private Character character;  // Now accepts a character via constructor
-    private int x, y;
+    private float x, y;
     private boolean movingLeft = false;
     private boolean movingRight = false;
     private boolean movingUp = false;
     private boolean movingDown = false;
+    private boolean attack = false;
     private final int SPEED = 60;
     private static final String WALKING_PATH = "characters/Characters(100x100)/Soldier/Soldier/Soldier-Walk.png";
     private static final String IDLE_PATH = "characters/Characters(100x100)/Soldier/Soldier/Soldier-Idle.png";
+    private static final String ATTACK_PATH = "characters/Characters(100x100)/Soldier/Soldier/Soldier-Attack02.png";
 
 
     public MovePerson(Character character) {
         this.character = character;
-//        this.x = character.GetX();
-//        this.y = character.GetY();
+        this.x = character.GetX();
+        this.y = character.GetY();
     }
+
+    public float GetX(){
+        return x;
+    }
+    public float GetY(){
+        return y;
+    }
+
 
     // Called when a key is pressed
     @Override
@@ -36,8 +48,14 @@ public class MovePerson  implements InputProcessor {
             movingUp = true; // Start moving up
         } else if (keycode == Input.Keys.S) {
             movingDown = true; // Start moving down
+        } else if (keycode == Input.Keys.SPACE){
+            attack = true;
         }
         return true;
+    }
+
+    boolean isAttacking(){
+        return attack;
     }
 
     // Called when a key is released
@@ -52,8 +70,20 @@ public class MovePerson  implements InputProcessor {
         } else if (keycode == Input.Keys.S) {
             movingDown = false; // Stop moving down
         }
+        else if (keycode == Input.Keys.SPACE){
+            attack = true;
+            Timer.schedule(new Timer.Task(){
+                @Override
+                public void run(){
+                    attack = false;
+                }
+            }, 0.5f);
+        }
         return true;
     }
+
+
+
 
     // Update class that moves the character in Render in Main
     public void update(float deltaTime) {
@@ -83,6 +113,9 @@ public class MovePerson  implements InputProcessor {
         // Change images if moving
         if (movingDown || movingUp || movingLeft || movingRight){
             character.SetFilePath(WALKING_PATH, 8);
+        }
+        else if (attack){
+            character.SetFilePath(ATTACK_PATH, 6);
         }
         else
         {
